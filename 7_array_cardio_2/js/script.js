f60e5e17abfcd739a78aa2b6acc43e46c9e29972
Shugmi Shumunov <SShumunov@gmail.com>
2017-11-11 18:53:34 -0500

    Add logic for array cardio day 2 lesson

--- 7_array_cardio_2/js/script.js
+++ 7_array_cardio_2/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
-  // ## Array Cardio Day 2
+
+	// ## Array Cardio Day 2
 
 	const people = [
 		{ name: 'Wes', year: 1988 },
@@ -17,13 +18,62 @@
 	];
 
 	// Some and Every Checks
+	// .some() checks to see if at least one things matches the criteria
 	// Array.prototype.some() // is at least one person 19 or older?
+
+	// const isAdult = people.some(function(person) {
+	// 	const currentYear = (new Date()).getFullYear();
+	// 	if (currentYear - person.year >= 19){
+	// 		return true;
+	// 	}
+	// })
+
+	const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+	console.log({isAdult});
+
+	// .everye() checks to see if all the things match the criteria
 	// Array.prototype.every() // is everyone 19 or older?
+
+	const isEveryoneAnAdult = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+	console.log({isEveryoneAnAdult});
 
 	// Array.prototype.find()
 	// Find is like filter, but instead returns just the one you are looking for
-  // find the comment with the ID of 823423
+	// find the comment with the ID of 823423
+
+	// const desiredComment = comments.find(function(comment){
+	// 	if (comment.id === 823423){
+	// 		return true;
+	// 	}
+	// })
+
+	const desiredComment = comments.find(comment => (comment.id === 823423));
+
+	console.log({desiredComment});
 
 	// Array.prototype.findIndex()
 	// Find the comment with this ID
 	// delete the comment with the ID of 823423
+
+	const indexOfComment = comments.findIndex(comment => comment.id === 823423);
+
+	console.log({indexOfComment});
+
+	// using splice
+	// comments.splice(indexOfComment, 1);
+	//
+	// console.table(comments);
+
+	// Creating a new array of comments
+	const newComments = [
+		...comments.slice(0, indexOfComment),
+		...comments.slice(indexOfComment + 1)
+	];
+
+	console.table(newComments);
+
+	// This keeps the old comments array intact as well
+
+	console.table(comments);
