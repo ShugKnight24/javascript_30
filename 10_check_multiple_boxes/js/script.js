74941c4f25b12c06b074828a23710017cdca43af
Shugmi Shumunov <SShumunov@gmail.com>
2017-11-18 17:09:07 -0500

    Add logic for multiple checkbox select lesson

--- 10_check_multiple_boxes/js/script.js
+++ 10_check_multiple_boxes/js/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const checkBoxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(event){
+
+	let inBetween = false;
+
+	if (event.shiftKey && this.checked) {
+
+		checkBoxes.forEach(checkbox => {
+
+			if (checkbox === this || checkbox === lastChecked) {
+				inBetween = !inBetween;
+			}
+
+			if (inBetween) {
+				checkbox.checked = true;
+			}
+
+		});
+
+	}
+
+	lastChecked = this;
+}
+
+checkBoxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
