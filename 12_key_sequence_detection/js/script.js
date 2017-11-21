61d4fbcb8096bdeee83a697128ac551309a7688b
Shugmi Shumunov <SShumunov@gmail.com>
2017-11-20 20:54:14 -0500

    Add logic for key sequence detection lesson

--- 12_key_sequence_detection/js/script.js
+++ 12_key_sequence_detection/js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const pressed = [];
+
+const secretCode = 'shugmishumunov';
+
+window.addEventListener('keyup', (event) => {
+	pressed.push(event.key);
+	pressed.splice(-secretCode.length - 1, (pressed.length - secretCode.length));
+
+	if (pressed.join('').includes(secretCode)){
+		console.log('Congrats for finding the Easter Egg');
+		cornify_add();
+	}
+
+	// console.log(event.key);
+	// console.log(pressed);
+});
