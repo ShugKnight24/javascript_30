cf033e0633211433a70e9c5daffdace5b4a5dff9
Shugmi Shumunov <SShumunov@gmail.com>
2021-03-22 22:17:33 -0400

    Update
    - play sound on click
    - remove animation on keyup
    - add helper function

--- 1_drum_kit/js/script.js
+++ 1_drum_kit/js/script.js
@@ -5,13 +5,16 @@ function playSound(event){
 	const key = document.querySelector(`.key[data-key="${ event.keyCode }"]`);
 
 	if (!audio) return;
-	// Allow multiple keypresses in quick succession
-	audio.currentTime = 0;
-	audio.play();
+	
+	resetAndPlayAudioFile(audio);
 	key.classList.add('playing');
 }
 
-document.addEventListener('keydown', playSound);
+document.addEventListener('keydown', playSound)
+document.addEventListener('keyup', function(event){
+	const key = document.querySelector(`.key[data-key="${ event.keyCode }"]`)
+	if(key && key.classList.contains('playing')) key.classList.remove('playing');
+})
 
 function removeTransition(event){
 	if (event.propertyName !== 'transform') return;
@@ -20,4 +23,23 @@ function removeTransition(event){
 
 const keys = document.querySelectorAll(`.key`);
 
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+keys.forEach(key => {
+	key.addEventListener('transitionend', removeTransition);
+});
+
+function playSoundOnClick(){
+	const data = this.dataset.key;
+	const audio = document.querySelector(`audio[data-key="${ data }"]`);
+	
+	resetAndPlayAudioFile(audio);
+	this.classList.add('playing');
+}
+
+keys.forEach(key => key.addEventListener('click', playSoundOnClick));
+
+function resetAndPlayAudioFile(audioElement){
+	// reseting to 0 allows multiple keypresses in quick succession
+	audioElement.currentTime = 0;
+	audioElement.play();
+}
+
