3d33e4e4260cc89aec1d8dcc4939beee1bb3dd73
Shugmi Shumunov <SShumunov@gmail.com>
2021-03-22 21:41:09 -0400

    Refactor old JS

--- 1_drum_kit/js/script.js
+++ 1_drum_kit/js/script.js
@@ -1,38 +1,23 @@
 'use strict';
 
 function playSound(event){
+	const audio = document.querySelector(`audio[data-key="${ event.keyCode }"]`);
+	const key = document.querySelector(`.key[data-key="${ event.keyCode }"]`);
 
-	const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-
-	const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-
-	if (!audio){
-
-		return;
-
-	}
-
+	if (!audio) return;
 	// Allow multiple keypresses in quick succession
 	audio.currentTime = 0;
 	audio.play();
 	key.classList.add('playing');
-
 }
 
+document.addEventListener('keydown', playSound);
+
 function removeTransition(event){
-
-	if (event.propertyName !== 'transform'){
-
-		return;
-
-	}
-
+	if (event.propertyName !== 'transform') return;
 	this.classList.remove('playing');
-
 }
 
 const keys = document.querySelectorAll(`.key`);
-
-window.addEventListener('keydown', playSound);
 
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
