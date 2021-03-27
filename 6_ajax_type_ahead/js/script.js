ff0d2a8640c54f347f2eec9d08e11194f82642b9
Shugmi Shumunov <SShumunov@gmail.com>
2021-03-27 17:08:19 -0400

    Refactor lesson 6

--- 6_ajax_type_ahead/js/script.js
+++ 6_ajax_type_ahead/js/script.js
@@ -5,14 +5,14 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 const cities = [];
 
 fetch(endpoint)
-	.then(blob => blob.json())
+	.then(res => res.json())
 	.then(data => cities.push(...data));
 
 function findMatches(wordToMatch, cities){
-	return cities.filter(place => {
+	return cities.filter(({ city, state }) => {
 		// Figure out if the city or state matches the entered text
-		const regex = new RegExp(wordToMatch, 'gi');
-		return place.city.match(regex) || place.state.match(regex);
+		const regex = new RegExp(wordToMatch, 'gi'); // global & case insensitive
+		return city.match(regex) || state.match(regex);
 	});
 }
 
@@ -22,14 +22,14 @@ function numberWithCommas(x){
 
 function displayMatches(){
 	const matchArray = findMatches(this.value, cities);
-	const html = matchArray.map(place => {
+	const html = matchArray.map(({ city, state, population}) => {
 		const regex = new RegExp(this.value, 'gi');
-		const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>` );
-		const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`)
+		const cityName = city.replace(regex, `<span class="hl">${ this.value }</span>` );
+		const stateName = state.replace(regex, `<span class="hl">${ this.value }</span>`)
 		return `
 			<li>
-				<span class="name">${cityName}, ${stateName}</span>
-				<span class="population">${numberWithCommas(place.population)}</span>
+				<span class="name">${ cityName }, ${ stateName }</span>
+				<span class="population">${ numberWithCommas(population) }</span>
 			</li>
 		`
 	}).join('');
