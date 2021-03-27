4fc266424f85162d1ea6bf89b7d3e8580a8498ea
Shugmi Shumunov <SShumunov@gmail.com>
2021-03-27 18:29:42 -0400

    Add map creation using openstreetmap and leaflet

--- 6_ajax_type_ahead/js/script.js
+++ 6_ajax_type_ahead/js/script.js
@@ -22,14 +22,21 @@ function numberWithCommas(x){
 
 function displayMatches(){
 	const matchArray = findMatches(this.value, cities);
-	const html = matchArray.map(({ city, state, population}) => {
+	const {latitude, longitude, city, state } = matchArray[0];
+	buildMap([latitude, longitude], city, state);
+
+	const html = matchArray.map(({ city, state, population, latitude, longitude }) => {
 		const regex = new RegExp(this.value, 'gi');
 		const cityName = city.replace(regex, `<span class="hl">${ this.value }</span>` );
 		const stateName = state.replace(regex, `<span class="hl">${ this.value }</span>`)
+		const latLong = [latitude, longitude];
 		return `
 			<li>
-				<span class="name">${ cityName }, ${ stateName }</span>
-				<span class="population">${ numberWithCommas(population) }</span>
+				<a class="build-map" onClick="buildMap([${ latLong }], '${ city }', '${ state }')">
+					<span class="name">${ cityName }, ${ stateName }</span> -
+					<span class="population-text">Pop:</span>
+					<span class="population">${ numberWithCommas(population) }</span>
+				</a>
 			</li>
 		`
 	}).join('');
@@ -41,3 +48,31 @@ const suggestions = document.querySelector('.suggestions');
 
 searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
+
+const mapContainer = document.querySelector('.map-container');
+
+function buildMap(latLong, city, state, mapZoom = 12){
+	if (mapContainer.classList.contains('hidden')){
+		console.log('hidden')
+		mapContainer.classList.toggle('hidden')
+	}
+	myMap.setView(latLong, mapZoom);
+	addCityAndState(city, state);
+}
+
+function addCityAndState(city, state){
+	document.querySelector('.map-title').innerHTML = `Map of ${ city }, ${ state }`;
+}
+
+// Setup BaseMap - OpenStreetMap & Leaflet
+const myMap = L.map('map');
+
+// basemap
+const myBasemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+myBasemap.addTo(myMap);
+
+let markers = L.layerGroup().addTo(myMap);
