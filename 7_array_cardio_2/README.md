fa51a7c78b3de32b0b7489fec35743295fb42f3d
Shugmi Shumunov <SShumunov@gmail.com>
2021-03-28 14:03:46 -0400

    Refactor lesson 7

--- 7_array_cardio_2/README.md
+++ 7_array_cardio_2/README.md
@@ -1,31 +1,27 @@
 # Array Cardio Day 2
 
+### 3/28/2021
+- Refactor JS
+  - Use destructuring, fix spacing, remove comments
+- Update README
 ### 11/11/2017
 
 **Thoughts**:
 
 Things I learned:
 
-- I learned that you can wrap a variable with curly brackets in a console.log to print the variable name along with the value of the variable.
-
+- I learned that you can wrap variables with curly brackets in a console.log to print the variable name along with the value of the variable.
   - This is super useful to me because I'll often write something like `console.log('variableName: ', variableName);`
-
   - I could just do `console.log({variableName});`
-
 - It was fun getting practice with `some()` and `every()`.
-
   - I've never used either before and I can see how both would be beneficial
-
-- `find()` and `findIndex()` were both really console
-
+- `find()` and `findIndex()`
   - I could definitely make use of these when I have the value I am searching against in an array
-
-- I need to get more practice using the `...` (spread operator)
-
-  - I need to look up some resources about the proper use so I can better understand when to use it
-
-- I enjoyed getting some practice shortening the longer functions into shorter arrows functions with implicit returns
+- I will get more practice using the `...` (spread operator)
+  - I will look up resources about the proper use so I can understand when to use it
+- I enjoy shortening the longer functions into shorter arrows functions with implicit returns
+  - Makes the code more readable
 
 ### Future Improvements
 
-- Like in the first instance of array cardio, I need to get better with ES6 in general. Going through this course, covering as much JavaScript as I possibly can, continuing to practice, and continuing to improve will lead me to getting better as a JavaScript dev. I'm excited to continue on this journey of growth.
+- Like in the first instance of array cardio, I will get better with ES6. Going through this course, covering as much JavaScript as possible, and continuing to practice will help me grow as a JavaScript dev. Excited to continue on this journey and complete this course
--- 7_array_cardio_2/js/script.js
+++ 7_array_cardio_2/js/script.js
@@ -1,79 +1,65 @@
 'use strict';
 
-	// ## Array Cardio Day 2
+// ## Array Cardio Day 2
+const people = [
+	{ name: 'Wes', year: 1988 },
+	{ name: 'Kait', year: 1986 },
+	{ name: 'Irv', year: 1970 },
+	{ name: 'Lux', year: 2015 }
+];
 
-	const people = [
-		{ name: 'Wes', year: 1988 },
-		{ name: 'Kait', year: 1986 },
-		{ name: 'Irv', year: 1970 },
-		{ name: 'Lux', year: 2015 }
-	];
+const comments = [
+	{ text: 'Love this!', id: 523423 },
+	{ text: 'Super good', id: 823423 },
+	{ text: 'You are the best', id: 2039842 },
+	{ text: 'Ramen is my fav food ever', id: 123523 },
+	{ text: 'Nice Nice Nice!', id: 542328 }
+];
 
-	const comments = [
-		{ text: 'Love this!', id: 523423 },
-		{ text: 'Super good', id: 823423 },
-		{ text: 'You are the best', id: 2039842 },
-		{ text: 'Ramen is my fav food ever', id: 123523 },
-		{ text: 'Nice Nice Nice!', id: 542328 }
-	];
+// Some and Every Checks
+// .some() checks to see if at least one things matches the criteria
+// Array.prototype.some() // is at least one person 19 or older?
 
-	// Some and Every Checks
-	// .some() checks to see if at least one things matches the criteria
-	// Array.prototype.some() // is at least one person 19 or older?
+const isAdult = people.some(({ year }) => ((new Date()).getFullYear()) - year >= 19);
 
-	// const isAdult = people.some(function(person) {
-	// 	const currentYear = (new Date()).getFullYear();
-	// 	if (currentYear - person.year >= 19){
-	// 		return true;
-	// 	}
-	// })
+console.log({ isAdult });
 
-	const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+// .everye() checks to see if all the things match the criteria
+// Array.prototype.every() // is everyone 19 or older?
 
-	console.log({isAdult});
+const isEveryoneAnAdult = people.every(({ year }) => ((new Date()).getFullYear()) - year >= 19);
 
-	// .everye() checks to see if all the things match the criteria
-	// Array.prototype.every() // is everyone 19 or older?
+console.log({ isEveryoneAnAdult });
 
-	const isEveryoneAnAdult = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
 
-	console.log({isEveryoneAnAdult});
+const desiredComment = comments.find(({ id }) => (id === 823423));
 
-	// Array.prototype.find()
-	// Find is like filter, but instead returns just the one you are looking for
-	// find the comment with the ID of 823423
+console.log({ desiredComment });
 
-	// const desiredComment = comments.find(function(comment){
-	// 	if (comment.id === 823423){
-	// 		return true;
-	// 	}
-	// })
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
 
-	const desiredComment = comments.find(comment => (comment.id === 823423));
+const indexOfComment = comments.findIndex(({ id }) => id === 823423);
 
-	console.log({desiredComment});
+console.log({ indexOfComment });
 
-	// Array.prototype.findIndex()
-	// Find the comment with this ID
-	// delete the comment with the ID of 823423
+// using splice
+// comments.splice(indexOfComment, 1);
+//
+// console.table(comments);
 
-	const indexOfComment = comments.findIndex(comment => comment.id === 823423);
+// Creating a new array of comments
+const newComments = [
+	...comments.slice(0, indexOfComment),
+	...comments.slice(indexOfComment + 1)
+];
 
-	console.log({indexOfComment});
+console.table(newComments);
 
-	// using splice
-	// comments.splice(indexOfComment, 1);
-	//
-	// console.table(comments);
+// This keeps the old comments array intact
 
-	// Creating a new array of comments
-	const newComments = [
-		...comments.slice(0, indexOfComment),
-		...comments.slice(indexOfComment + 1)
-	];
-
-	console.table(newComments);
-
-	// This keeps the old comments array intact as well
-
-	console.table(comments);
+console.table(comments);
