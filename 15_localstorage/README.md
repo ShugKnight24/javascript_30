ed6c95d7d2d3a2cda43f00634f48a6567cca9acb
Shugmi Shumunov <SShumunov@gmail.com>
2021-04-05 16:06:39 -0400

    Refactor lesson 15

--- 15_localstorage/README.md
+++ 15_localstorage/README.md
@@ -1,29 +1,27 @@
 # LocalStorage and Event Delegation
 
-### 11/25/2017
+## 4/5/2021
+- Refactor JS
+- Add delete for individual items
+- Add Check All, add uncheck all, add delete all
+- Update README
 
-**Thoughts**: I enjoyed going through this lesson and learning about localStorage. I was familiar with the concept, but never implemented anything with localStorage in the past. I am pretty familiar with event delegation due to my use of jQuery. I remember those first few times I was dynamically generating HTML through JavaScript and couldn't wrap my head around why I couldn't select or target those dynamically generated elements.
+## 11/25/2017
+
+**Thoughts**: Great going through this and learning localStorage. I knew localStorage could be used persist data, but never built anything with it or understood how it worked under the hood. I'm familiar with event delegation thanks to JavaScript, & jQuery. When I first learned JavaScript, I couldn't wrap my head around selecting or targeting dynamically generated elements. Now it makes more sense
 
 Things I learned:
 
-- Setting up and implementing localStorage in a project.
+- Setting up localStorage in a project.
+- Examples of `JSON.parse()` and `JSON.stringify()`
+- Using `content:before` & `content:after`
+  - Leveraging this technique may be beneficial and flexible for content changes
 
-- I've used both `JSON.parse()` and `JSON.stringify()` in the past, but I enjoyed seeing the use cases in this example.
+## Future Improvements
 
-- At work, I've hid the default checkbox inputs and created custom ones, but I didn't use `content:before` / `content:after`.
+- ~~Adding a check all button~~ - ✅
+- ~~Adding an uncheck all button~~ - ✅
+- ~~Adding a delete all button~~ - ✅
+- Create a small menu that people can select 'popular' food choices to populate the list.
 
-  - This may be a better solution to implement and I will have to revisit it. It seems to be more clean and less hacky then the manner in which I did it.
-
-### Future Improvements
-
-Even before Wes said these ideas, I wondered about implementing them.
-
-- Adding a check all button
-
-- Adding an uncheck all button
-
-- Adding a delete all button
-
-These would be fun to implement and tying them in with localStorage would be a fun test to see if I truly learned the material.
-
-- I also want to create a small menu that people can select 'popular' food choices from that will populate their own list.
+These will be fun to implement
--- 15_localstorage/js/script.js
+++ 15_localstorage/js/script.js
@@ -3,6 +3,7 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem('items')) || [];
+let isItems = (items.length > 0) ? true : false;
 
 function addItem(event){
 
@@ -15,41 +16,34 @@ function addItem(event){
 	};
 
 	items.push(item);
-
-	populateList(items, itemsList);
-
-	localStorage.setItem('items', JSON.stringify(items));
-
+	isItems = (items.length > 0) ? true : false;
+	showHideActions();
+	buildAndSave();
 	this.reset();
-
 }
 
 function toggleDone(event){
 	if (!event.target.matches('input')) return;
 
 	const element = event.target;
-
 	const index = element.dataset.index;
 
 	items[index].done = !items[index].done;
-
-	localStorage.setItem('items', JSON.stringify(items));
-
-	populateList(items, itemsList);
-
+	
+	buildAndSave();
 }
 
 function populateList(plates = [], platesList){
 
-	platesList.innerHTML = plates.map((plate, i) => {
+	platesList.innerHTML = plates.map(({done, text}, i) => {
 		return `
 			<li>
-				<input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
-				<label for="item${i}">${plate.text}</label>
+				<input type="checkbox" data-index=${ i } id="item${ i }" ${ done ? 'checked' : '' } />
+				<label for="item${ i }">${ text }</label>
+				<i class="far fa-trash-alt delete-plate" data-index=${ i }></i>
 			</li>
 		`;
 	}).join('');
-
 }
 
 addItems.addEventListener('submit', addItem);
@@ -57,3 +51,59 @@ addItems.addEventListener('submit', addItem);
 itemsList.addEventListener('click', toggleDone);
 
 populateList(items, itemsList);
+
+function buildAndSave(){
+	populateList(items, itemsList);
+	localStorage.setItem('items', JSON.stringify(items));
+}
+
+document.addEventListener('click', function(event){
+	let target = event.target;
+	let index = target.dataset.index;
+	if (!target.classList.contains('delete-plate')) return;
+
+	deletePlate(index);
+	isItems = (items.length > 0) ? true : false;
+	showHideActions();
+	buildAndSave();
+});
+
+function deletePlate(currentIndex){
+	items.splice(currentIndex, 1);
+}
+
+const actions = document.querySelector('.actions');
+
+function showHideActions(){
+	if (isItems){
+		actions.classList.add('active');
+	} else {
+		actions.classList.remove('active');
+	}
+}
+
+// run if there are items in localStorage
+showHideActions();
+
+const checkAll = document.querySelector('.check-all');
+const deleteAll = document.querySelector('.delete-all');
+
+checkAll.addEventListener('click', checkUncheckAll);
+deleteAll.addEventListener('click', deleteAllItems);
+
+let allChecked = false;
+
+function checkUncheckAll(){
+	items.forEach(item => {
+		allChecked ? item.done = false :item.done = true;
+	});
+	allChecked = !allChecked;
+	buildAndSave();
+}
+
+function deleteAllItems(){
+	items.splice(0);
+	buildAndSave();
+	isItems = false;
+	showHideActions();
+}
