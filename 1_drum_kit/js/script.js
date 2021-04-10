1f14b83d67f5b096806b67fb6e86d258fcf3827d
Shugmi Shumunov <SShumunov@gmail.com>
2021-04-09 21:38:30 -0400

    Fix whitespace and add missing semicolons

--- 1_drum_kit/js/script.js
+++ 1_drum_kit/js/script.js
@@ -5,16 +5,16 @@ function playSound(event){
 	const key = document.querySelector(`.key[data-key="${ event.keyCode }"]`);
 
 	if (!audio) return;
-	
+
 	resetAndPlayAudioFile(audio);
 	key.classList.add('playing');
 }
 
-document.addEventListener('keydown', playSound)
+document.addEventListener('keydown', playSound);
 document.addEventListener('keyup', function(event){
 	const key = document.querySelector(`.key[data-key="${ event.keyCode }"]`)
 	if(key && key.classList.contains('playing')) key.classList.remove('playing');
-})
+});
 
 function removeTransition(event){
 	if (event.propertyName !== 'transform') return;
@@ -23,14 +23,12 @@ function removeTransition(event){
 
 const keys = document.querySelectorAll(`.key`);
 
-keys.forEach(key => {
-	key.addEventListener('transitionend', removeTransition);
-});
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 function playSoundOnClick(){
 	const data = this.dataset.key;
 	const audio = document.querySelector(`audio[data-key="${ data }"]`);
-	
+
 	resetAndPlayAudioFile(audio);
 	this.classList.add('playing');
 }
@@ -42,4 +40,3 @@ function resetAndPlayAudioFile(audioElement){
 	audioElement.currentTime = 0;
 	audioElement.play();
 }
-
